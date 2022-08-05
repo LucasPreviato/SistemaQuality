d56b18a97e11bb466caf69a543b239d2548f2d96
Lucas Previato <lucaspreviato@live.com>
2022-08-05 16:54:44 -0300

    sidebar 100%

--- app.quality.frontend/src/components/Sidebar/index.tsx
+++ app.quality.frontend/src/components/Sidebar/index.tsx
@@ -2,12 +2,23 @@ import { NavLink } from 'react-router-dom'
 import { Popover } from '@headlessui/react'
 import { Button } from '../Button'
 
-import rhImg from '../../assets/img/sidebar/iconRH.svg'
-import auditoriaImg from '../../assets/img/sidebar/iconAuditoria.svg'
-import equipamentoImg from '../../assets/img/sidebar/iconEquipamento.svg'
-import fornecedoresImg from '../../assets/img/sidebar/iconFornecedores.svg'
 import iconRotina from '../../assets/img/sidebar/iconRotina.svg'
+import iconRH from '../../assets/img/sidebar/iconRH.svg'
+import iconEquipamento from '../../assets/img/sidebar/iconEquipamento.svg'
+import iconAuditoria from '../../assets/img/sidebar/iconAuditoria.svg'
+import iconFornecedores from '../../assets/img/sidebar/iconFornecedores.svg'
 import iconCadastro from '../../assets/img/sidebar/iconCadastro.svg'
+import iconRelatorios from '../../assets/img/sidebar/iconRelatorios.svg'
+import iconIndicadores from '../../assets/img/sidebar/iconIndicadores.svg'
+import iconDocumentos from '../../assets/img/sidebar/iconDocumentos.svg'
+import iconContratos from '../../assets/img/sidebar/iconContratos.svg'
+import iconManutencao from '../../assets/img/sidebar/iconManutencao.svg'
+import iconTecnicos from '../../assets/img/sidebar/iconTecnicos.svg'
+import iconControles from '../../assets/img/sidebar/iconControles.svg'
+import iconChamados from '../../assets/img/sidebar/iconChamados.svg'
+import iconOutros from '../../assets/img/sidebar/iconOutros.svg'
+import iconTI from '../../assets/img/sidebar/iconTI.svg'
+import iconMedicos from '../../assets/img/sidebar/iconMedicos.svg'
 
 import logoImg from '../../assets/SysQuali.ico'
 
@@ -18,22 +29,22 @@ const BUTTONS_INFO = [
     path: '/rotina',
   },
   {
-    src: rhImg,
+    src: iconRH,
     title: 'RH',
     path: '/rh',
   },
   {
-    src: equipamentoImg,
+    src: iconEquipamento,
     title: 'Equipamento',
     path: '/equipamento',
   },
   {
-    src: auditoriaImg,
+    src: iconAuditoria,
     title: 'Auditoria',
     path: '/auditoria',
   },
   {
-    src: fornecedoresImg,
+    src: iconFornecedores,
     title: 'Fornecedores',
     path: '/fornecedores',
   },
@@ -45,67 +56,67 @@ const BUTTONS_INFO = [
   },
 
   {
-    src: iconCadastro,
+    src: iconRelatorios,
     title: 'Relatórios',
     path: '/relatorios',
   },
 
   {
-    src: iconCadastro,
+    src: iconIndicadores,
     title: 'Indicadores',
     path: '/indicadores',
   },
 
   {
-    src: iconCadastro,
+    src: iconDocumentos,
     title: 'Documentos',
     path: '/documentos',
   },
 
   {
-    src: iconCadastro,
+    src: iconContratos,
     title: 'Contratos',
     path: '/contratos',
   },
 
   {
-    src: iconCadastro,
+    src: iconManutencao,
     title: 'Manutenção',
     path: '/manutencao',
   },
 
   {
-    src: iconCadastro,
+    src: iconTecnicos,
     title: 'Técnicos',
     path: '/tecnicos',
   },
 
   {
-    src: iconCadastro,
+    src: iconControles,
     title: 'Controles',
     path: '/controles',
   },
 
   {
-    src: iconCadastro,
+    src: iconChamados,
     title: 'Chamados',
     path: '/chamados',
   },
 
   {
-    src: iconCadastro,
+    src: iconOutros,
     title: 'Outros',
     path: '/outros',
   },
 
   {
-    src: iconCadastro,
+    src: iconTI,
     title: 'TI',
     path: '/ti',
   },
 
   {
-    src: iconCadastro,
+    src: iconMedicos,
     title: 'Médicos',
     path: '/medicos',
   },
