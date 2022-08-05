fa55cce00a38668bc2a0143ee3b509eb6d0c40bd
Matheus Henrique <mattsilvahenrique102@gmail.com>
2022-08-04 21:49:55 -0300

    Componentização da tabela 90%

--- app.quality.frontend/src/components/Cadastro/Colaboradores/CargosFuncao/index.tsx
+++ app.quality.frontend/src/components/Cadastro/Colaboradores/CargosFuncao/index.tsx
@@ -2,42 +2,22 @@ import { StandardTable } from '../../../StandardTable'
 
 const theadTitle = [
   {
-    title: 'Título',
+    title: 'titulo',
   },
   {
-    title: 'Título',
+    title: 'titulo',
   },
   {
-    title: 'Título',
+    title: 'titulo',
   },
   {
-    title: 'Título',
+    title: 'titulo',
   },
   {
-    title: 'Título',
+    title: 'titulo',
   },
   {
-    title: 'Título',
-  },
-]
-const tbodyTitle = [
-  {
-    title: 'Título',
-  },
-  {
-    title: 'Título',
-  },
-  {
-    title: 'Título',
-  },
-  {
-    title: 'Título',
-  },
-  {
-    title: 'Título',
-  },
-  {
-    title: 'Título',
+    title: 'titulo',
   },
 ]
 
@@ -45,25 +25,16 @@ export function CargosFuncao() {
   return (
     <div className="w-full md:p-12 p-10">
       <div className="p-6 bg-dark-500 rounded-md">
-        <StandardTable>
-          <thead>
-            <tr className="grid grid-cols-8 place-items-center rounded-t bg-teal-500">
-              {theadTitle.map((thead) => (
-                <th key={thead.title}>{thead.title}</th>
-              ))}
+        <StandardTable
+          tbodyChildren={theadTitle.map((thead) => (
+            <th key={thead.title}>{thead.title}</th>
+          ))}
+          theadChildren={theadTitle.map((tbody) => (
+            <tr key={tbody.title}>
+              <td>{tbody.title}</td>
             </tr>
-          </thead>
-          <tbody className="max-h-40  overflow-auto scrollbar-none grid gap-2 border-collapse border-x-2 border-b-2 rounded-b border-dark-600">
-            {tbodyTitle.map((tbody) => (
-              <tr
-                key={tbody.title}
-                className="grid grid-cols-8  place-items-center py-2 hover:bg-dark-400 transition-colors"
-              >
-                <td>{tbody.title}</td>
-              </tr>
-            ))}
-          </tbody>
-        </StandardTable>
+          ))}
+        />
       </div>
     </div>
   )
