eb264c8db81f820342e7277d900b87ef43e52401
Lucas Previato <lucaspreviato@live.com>
2022-08-06 22:47:34 -0300

    organizing routes and registration buttons

--- app.quality.frontend/src/App.tsx
+++ app.quality.frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Router'
+import { Router } from './routes'
 
 export function App() {
   return (
--- app.quality.frontend/src/components/SelectionPanel.tsx
+++ app.quality.frontend/src/components/SelectionPanel.tsx
@@ -7,8 +7,8 @@ interface SelectionPanelProps {
 
 export function SelectionPanel({ children }: SelectionPanelProps) {
   return (
-    <div className="w-full md:p-12 p-10">
-      <Popover className="h-full bg-dark-600 rounded p-6 grid gap-4 grid-cols-6 place-items-center place-content-start">
+    <div className="w-full md:p-12 p-10 ">
+      <Popover className="h-full h-max-full bg-dark-600 rounded p-6 grid gap-4 grid-cols-6 place-items-center place-content-start">
         {children}
       </Popover>
     </div>
